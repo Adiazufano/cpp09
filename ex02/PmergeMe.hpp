3b6e7dbb43c8c9bf50b03a13be4b8fc496673c3f
Alberto <adiazufanomarti@gmail.com>
2026-03-13 17:25:17 +0100

    add pmergeme

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,8 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+#include <iostream>
+#include <string.h>
+
+using std::cout;
+using std::endl;
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,12 @@
+#include "PmergeMe.hpp"
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	if (argc < 2)
+	{
+		cout << "Error: necesitas al menos 1 argumento" << endl;
+		return(1);
+	}
+	return(0);
+}
